e2b007f91ae377347d7e89f051fb4a1f6879aa51
eshaan2411 <84580198+eshaan2411@users.noreply.github.com>
2021-09-13 21:56:45 +0530

    Add files via upload

---
+++ my-distributions-package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probabilistic-distributions',
+      version='1.0',
+      description='Gaussian and Binomial distribution estimator',
+      packages=['distributions'],
+      author = "Eshaan Saraf", \
+      author_email = "eshaan428@gmail.com",
+      zip_safe=False)
